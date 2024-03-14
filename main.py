b2351048bf3680acb1286e1bbbf7aba2c3555818
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2024-03-14 15:17:53 -0700

    Log data if the data is raining

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ weather_params = {
   "lat": 37.338207,
   "lon": -121.886330,
   "appid": API_key,
+  "cnt": 4,
+  "units": "imperial",
 }
 
 weather_res = requests.get(URL_forecast, params=weather_params)
@@ -14,6 +16,6 @@ weather_res.raise_for_status()
 data_json = weather_res.json()
 
 for data in data_json["list"]:
-  if data["weather"][0]["main"] == "Clouds":
+  if data["weather"][0]["id"] < 600:
     date_time = data["dt_txt"].split()
-    print(date_time)
+    print(data)
