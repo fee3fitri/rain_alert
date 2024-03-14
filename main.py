40e2b4a3ddb507b1b221b85f4c383badb4bfb9f6
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2024-03-14 15:48:25 -0700

    Add Send SMS API

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import requests
+import urllib.request
+import urllib.parse
 
-API_key = "8808e4b20388400281ba6b66634819b2"
+# ------- Sends SMS Notification ------- #
+def sendSMS(sms_api, numbers, sender, message):
+  data = urllib.parse.urlencode({
+    "apikey": sms_api,
+    "numbers": numbers,
+    "message": message,
+    "sender": sender
+  })
+
+  data = data.encode("utf-8")
+  request = urllib.request.Request("https://api.txtlocal.com/send/?")
+  f = urllib.request.urlopen(request,data)
+  fr = f.read()
+  return fr
+
+
+# ------- Getting weather data ------- #
+SMS_API_key = "NmI2YTVhN2E2OTQzN2EzMDY4Njg0MTRkNzQ0OTRhNzE="
+weather_API_key = "8808e4b20388400281ba6b66634819b2"
 URL_forecast = "https://api.openweathermap.org/data/2.5/forecast"
 
-weather_params = {
-  "lat": 37.338207,
-  "lon": -121.886330,
-  "appid": API_key,
+weather_params = { # Tulsa location
+  "lat": 36.153980,
+  "lon": -95.992775,
+  "appid": weather_API_key,
   "cnt": 4,
   "units": "imperial",
 }
@@ -17,5 +37,4 @@ data_json = weather_res.json()
 
 for data in data_json["list"]:
   if data["weather"][0]["id"] < 600:
-    date_time = data["dt_txt"].split()
-    print(data)
+    [date, time] = data["dt_txt"].split()
