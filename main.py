e2733e08d1093a23e3442c67b430ffb1cf84d524
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2024-03-14 15:06:15 -0700

    Add prediction for cloudy time

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ API_key = "8808e4b20388400281ba6b66634819b2"
 URL_forecast = "https://api.openweathermap.org/data/2.5/forecast"
 
 weather_params = {
-  "lat": 44.34,
-  "lon": 10.99,
+  "lat": 37.338207,
+  "lon": -121.886330,
   "appid": API_key,
 }
 
 weather_res = requests.get(URL_forecast, params=weather_params)
 weather_res.raise_for_status()
-data = weather_res.json()
+data_json = weather_res.json()
 
-print(data)
+for data in data_json["list"]:
+  if data["weather"][0]["main"] == "Clouds":
+    date_time = data["dt_txt"].split()
+    print(date_time)
